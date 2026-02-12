82e9d05e906ea923bf97699d1e7a36c6e6a50d65
CrimsonKnight9 <161537678+CrimsonKnight9@users.noreply.github.com>
2026-02-11 22:52:19 -0500

    Added setting for Dyson Sphere compatibility
    New settings are not yet functional

--- keep-space-connections/settings.lua
+++ keep-space-connections/settings.lua
@@ -5,5 +5,19 @@ data:extend({
         name = "choosing-connections-to-keep",
         default_value = "nauvis-vulcanus, nauvis-fulgora, nauvis-gleba",
         allow_blank = true
+    },
+    {
+        setting_type = "startup",
+        type = "string-setting",
+        name = "planets-to-ignore",
+        default_value = "",
+        allow_blank = true
+    },
+    {
+        setting_type = "startup",
+        type = "string-setting",
+        name = "progression-fix-dyson-sphere",
+        default_value = "Full",
+        allowed_values = {"Full", "Close Sun Orbit", "None"}
     }
 })
